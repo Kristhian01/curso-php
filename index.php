c02c928a14e3b70569b44e89c4612ddf328422d0
Arce <elsami45@hotmail.com>
2024-06-26 20:22:35 +0200

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 const API_URL = "https://whenisthenextmcufilm.com/api";
 #Inicializar una nueva sesión de cURL; ch = cURL handle
 $ch = curl_init(API_URL);
-//Indicar quq queremos recibir el resultado de la petición y nomostrarla en pantalla
+//Indica que queremos recibir el resultado de la petición y no mostrarla en pantalla
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 /*Ejecutar la petición
  y guardamos el resultado
@@ -25,19 +25,19 @@ var_dump($data);
 </head>
 
 <main>
-    
-        <pre style="font-style: 8px; overflow:scroll; height: 250px;">
+
+    <pre style="font-style: 8px; overflow:scroll; height: 250px;">
           <?php var_dump($data); ?>
          </pre>
-        <h2>La próxima película de Marvel</h2>
+    <h2>La próxima película de Marvel</h2>
     <section>
         <img src="<?= $data["poster_url"]; ?>" width="300" alt="poster de <?= $data["title"]; ?>" style="border-radius:16px">
     </section>
 
     <hgroup>
-        <h3><?= $data["title"]; ?>se entrena en <?= $data["days_until"];?> dias</h3>
+        <h3><?= $data["title"]; ?>se entrena en <?= $data["days_until"]; ?> dias</h3>
         <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
-        <p>La siguiente es: <?=$data["following_production"]["title"]; ?></p>
+        <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
     </hgroup>
 </main>
 
@@ -53,21 +53,24 @@ var_dump($data);
         :root {
             color-scheme: light dark;
         }
-        section{
-            display:flex;
+
+        section {
+            display: flex;
             justify-content: center;
             text-align: center;
         }
-        hgroup{
+
+        hgroup {
             display: flex;
             flex-direction: column;
             justify-content: center;
             text-align: center;
         }
 
-        img{
+        img {
             margin: 0 auto;
         }
+
         body {
             display: grid;
             place-content: center;
